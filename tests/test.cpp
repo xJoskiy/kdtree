f0d5c4424c62cfdd6d34906b70affabbdb7f6ade
xJoskiy <pavel5553-20@mail.ru>
2024-07-29 20:27:56 +0300

    Add tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -6,42 +6,70 @@
 #include "kdtree.h"
 
 template <class T>
-struct point : std::vector<T> {
+class point : public std::vector<T> {
     size_t dim;
 
+public:
+    size_t get_dim() const {
+        return dim;
+    };
+
     point(std::initializer_list<T> list) : std::vector<T>(list), dim(list.size()) {}
+
+    point(const std::vector<T>& vec) : std::vector<T>(vec), dim(vec.size()) {}
+
+    bool operator==(const point& p) const {
+        return static_cast<const std::vector<T>&>(p) == static_cast<const std::vector<T>&>(*this);
+    }
 };
 
 TEST(test_kdtree, test_insertion) {
-    std::vector<point<int>> points{{2, 3}, {4, 7}, {5, 4}, {7, 2}, {8, 1}, {9, 6}};
+    std::vector<point<int>> points{{2, 3}, {4, 7}, {5, 4}, {7, 2}};  //, {8, 1}, {9, 6}};
     kdtree<point<int>> tree(points);
-    tree.insert(point<int>({1, 2}));
-    tree.insert(point<int>({2, 3}));
-    tree.insert(point<int>({4, 7}));
 }
 
 TEST(test_kdtree, test_size) {
     std::vector<point<int>> points{{2, 3}, {4, 7}, {5, 4}, {7, 2}, {8, 1}, {9, 6}};
     kdtree<point<int>> tree(points);
-    tree.insert(point<int>({1, 2}));
-    tree.insert(point<int>({2, 3}));
-    tree.insert(point<int>({4, 7}));
+    point<int> p({2, 2});
+    tree.insert(p);
+    tree.insert(p);
 
-    ASSERT_EQ(9, tree.size());
+    ASSERT_EQ(8, tree.size());
 }
 
-// не все точки в диапазоне + ошибки valgrind + точки задваиваются
-// чини :)
+TEST(test_kdtree, test_root_query) {
+    std::vector<point<int>> points;
+    kdtree<point<int>> tree;
+    point<int> p({2, 2});
+    tree.insert(p);
+
+    auto res = tree.query_search({1, 1}, {3, 3});
+    std::vector<point<int>> expected{{2, 2}};
+    EXPECT_EQ(expected, res);
+}
 
 TEST(test_kdtree, test_query) {
     std::vector<point<int>> points{{2, 3}, {4, 7}, {5, 4}, {7, 2}, {8, 1}, {9, 6}};
     kdtree<point<int>> tree(points);
-    tree.insert(point<int>({1, 2}));
-    tree.insert(point<int>({2, 3}));
-    tree.insert(point<int>({4, 7}));
+    point<int> p({-24, 10});
+    tree.insert(p);
+    tree.insert(p);
 
     auto res = tree.query_search({1, 2}, {9, 6});
-    for (auto x : res) {
-        std::cout << x[0] << ' ' << x[1] << std::endl;
-    }
+    std::vector<point<int>> expected{{2, 3}, {5, 4}, {7, 2}, {9, 6}};
+    EXPECT_EQ(expected, res);
+}
+
+TEST(test_kdtree, test_as_vector) {
+    std::vector<point<int>> points{{2, 3}, {4, 7}, {5, 4}, {7, 2}, {8, 1}, {9, 6}};
+    kdtree<point<int>> tree(points);
+    point<int> p({2, 2});
+    tree.insert(p);
+    tree.insert(p);
+
+    auto res = tree.as_vector();
+    std::vector<point<int>> expected{{2, 3}, {4, 7}, {5, 4}, {7, 2},
+                                     {8, 1}, {9, 6}, {2, 2}, {2, 2}};
+    EXPECT_EQ(expected, res);
 }
